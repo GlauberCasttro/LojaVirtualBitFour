3a3ba9e3782b5040acd3e4afb119c1726e0cc147
glauberBitfour <grobis_mn@hotmail.com>
2017-02-27 14:27:50 -0300

    teste

---
+++ BitFour.LojaVirtual.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitFour.LojaVirtual.Test
+{
+    [TestClass]
+    public class BitFourUnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
