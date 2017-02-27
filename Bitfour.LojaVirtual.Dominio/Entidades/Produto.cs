34c698e593ae549a8c7f7659887e72efd46230b8
glauberBitfour <grobis_mn@hotmail.com>
2017-02-27 14:45:17 -0300

    Class Produto

---
+++ Bitfour.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitfour.LojaVirtual.Dominio.Entidades
+{
+   public class Produto
+    {
+        public int ProdudtoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string categoria { get; set; }
+    }
+}
